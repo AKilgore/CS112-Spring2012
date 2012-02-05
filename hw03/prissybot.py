df2deaffe3d84e94962ce421b31114be3a244ccd
Aislynn Kilgore <mak11@hampshire.edu>
2012-02-05 16:41:07 -0500

    hw03 done

--- hw03/prissybot.py
+++ hw03/prissybot.py
@@ -1,48 +1,52 @@
 #!/usr/bin/env python
-"""
-prissybot.py
+print "Hello! I am Prissy Bot! What is your name?"
+name=raw_input ()
 
-CS112 Homework 3:   PrissyBot
+print "You mean, Hi bot,sir",name
 
-Prissy bot, the rude chat bot, is just mean!  It does not listen, asks obnoxious questions, and says anything it likes.
-"""
+print "Why are you so rude?"
+rude=raw_input ()
 
-# Step 1:
-# -----------------------
-# Program the following.
-# 
-#    $ python prissybot.py
-#    Enter your name:  Paul
-#   
-#    PrissyBot: Hello there, Paul
-#    Paul: hi bot
-#    PrissyBot: You mean, "hi bot, sir!"
-# 
-# Make sure the user inputs their own name and responses.
+print "There are certainly better reasons to be so rude than just" ,rude
 
+print "But I guess that's about the unrefinement I would expect from someone named" ,name
 
+print "Okay, if you do some math problems correctly, I will give you a present, just because I feel bad for such a monocle-less simplton such as you" ,name
 
-# Step 2:
-# -----------------------
-# Keep adding to the conversation. Make sure that your program 
-# includes the following:
-# 
-#  * get and use input from the user
-#  * 3 math problems
-#     * at least one should get numbers from the user
-#  * at least 3 insults
+print "Is this okay with you" ,name
+mathanswer=raw_input()
 
+print "Well you're doing this math whether you like it or not!"
 
-# Advanced
-# -------------------------
-# Make sure your prissy bot uses string formatting throughout.  
-# Also, create new programs for the following:
-#  
-#  1. draw some kind of ascii art based on user input
-#  2. print a decimal/binary/hexidecimal conversion table 
-#     * well formated and labeled
-#     * reads 5 numbers from the input (all less than 256)
-#  3. reduce a fraction
-#     * read a numerator and denominator from the user
-#     * ex.  6/4 = 1 2/4
+print "Enter a number:"
+n=raw_input ()
+
+print "Enter another number:"
+m=raw_input ()
+
+n=int(n)
+m=float(m)
+
+print n,"+",m,"=",n+m
+print n,"-",m,"=",n-m
+print n,"*",m,"=",n*m
+print n,"/",m,"=",n/m
+
+print "Congratulations! (even though I did most of the work) Now you get your diamond!"
+
+print "What will your diamond be made of?"
+s=" "
+c=raw_input ()
+
+print 4*s,1*c
+print 3*s,3*c
+print 2*s,5*c
+print 1*s,7*c
+print 0*s,9*c
+print 1*s,7*c
+print 2*s,5*c
+print 3*s,3*c
+print 4*s,1*c
+
+print "Now go away."
 
